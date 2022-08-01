b73586b7de5a2188b6a033ce5932f95ff8fde908
valdrin-12 <109156924+valdrin-12@users.noreply.github.com>
2022-08-01 13:43:05 +0200

    add checkboctextbox
    text box

---
+++ checkBox/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+class EntryPoint
+
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement? textBox;
+    static void Main()
+
+    {
+        string url = "https://automationintesting.com/selenium/testpage/";
+        driver.Navigate().GoToUrl(url);
+        textBox = driver.FindElement(By.CssSelector("#firstname"));
+        textBox.SendKeys("sheno emailin test");
+        Thread.Sleep(3000);
+
+        driver.Quit();
+
+
+
+    }// ok
+}
+
